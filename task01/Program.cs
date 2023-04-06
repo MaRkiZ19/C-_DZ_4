e248d198c5ec76a2551d788ea9b814afeb1e35ad
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-06 19:55:41 +0300

    add tasks

---
+++ task01/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+int Read()
+{
+    System.Console.Write("Number : ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int a = Read();
+int b = Read();
+int stepen = b;
+//for (int i=1; i<=n; i++)
+     Console.WriteLine("a^b = " + Math.Pow(a, stepen));
