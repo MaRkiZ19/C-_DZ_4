a0f5c81d02af57a80d7ba5d7901d50460f6b378d
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-09 23:14:55 +0300

    not done task 02

---
+++ task02/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*int Read()
+{
+    System.Console.WriteLine("Enter number: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int number = Read();
+System.Console.WriteLine();
+string num = number.ToString();
+
+
+
+string main(string num)
+    {
+        char[] array = num.ToCharArray();
+        return num;
+    }
+
+
+/*int [] CreateArray(int min, int max)
+{
+int [] numbers = new int [b];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(min,max +1);
+}
+return numbers;
+}
+
+void PrintArray(int [] (num)
+{
+System.Console.Write("[");
+for (int i = 0; i < numbers.Length -1; i++)
+{
+    System.Console.Write(numbers[i] + "; ");
+}
+System.Console.WriteLine(numbers[numbers.Length -1] + "]");
+}
+
+/*int Num (string arg)
+{
+    System.Console.WriteLine($"Введите {arg} ");
+    return int.Parse(System.Console.ReadLine());
+}
+
+int min = Num("меньшее число");
+int max = Num("максимальное число");
+int [] numbers = CreateArray(min, max);
+PrintArray(numbers);*/
